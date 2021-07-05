72af59005d09fac9e12aeefe15b567976728c8ca
Jason Rigby <jason@jasonrig.by>
2021-07-06 01:35:25 +0900

    add grpc stubs

--- setup.py
+++ setup.py
@@ -1,6 +1,34 @@
 import itertools
+import os.path
 
-from setuptools import setup
+from setuptools import setup, Command
+
+setup_path = os.path.dirname(os.path.realpath(__file__))
+
+class GrpcBuild(Command):
+
+    GRPC_PROTO_PATH = os.path.join(setup_path, "protobuf")
+    PYTHON_OUT_PATH = os.path.join(setup_path, "paramiko_cloud", "protobuf")
+
+    description = "build the grpc interface from proto schemas"
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import grpc_tools.protoc
+        grpc_tools.protoc.main([
+            'grpc_tools.protoc',
+            '-I{}'.format(self.GRPC_PROTO_PATH),
+            '--python_out={}'.format(self.PYTHON_OUT_PATH),
+            '--grpc_python_out={}'.format(self.PYTHON_OUT_PATH),
+            os.path.join(self.GRPC_PROTO_PATH, "rpc.proto")
+        ])
 
 extras_require = {
     "aws": ["boto3"],
@@ -12,10 +40,13 @@ extras_require = {
 }
 extras_require["all"] = list(itertools.chain(*extras_require.values()))
 
+
+
 setup(
     name='paramiko-cloud',
     version='1.0',
     packages=['paramiko_cloud', 'paramiko_cloud.aws', 'paramiko_cloud.gcp'],
+    include_package_data=True,
     url='',
     license='MIT',
     author='Jason Rigby',
@@ -24,16 +55,21 @@ setup(
     setup_requires=[
         "protobuf_distutils"
     ],
+    cmdclass={
+        "build_grpc": GrpcBuild
+    },
     options={
         "generate_py_protobufs": {
-            "source_dir": "./paramiko_cloud/protobuf",
-            "proto_root_path": "."
+            "source_dir": os.path.join(setup_path, "protobuf"),
+            "proto_root_path": os.path.join(setup_path, "protobuf"),
+            "output_dir": os.path.join(setup_path, "paramiko_cloud", "protobuf")
         }
     },
     install_requires=[
         "paramiko",
         "cryptography",
-        "protobuf"
+        "protobuf",
+        "grpcio-tools"
     ],
     extras_require=extras_require
 )
